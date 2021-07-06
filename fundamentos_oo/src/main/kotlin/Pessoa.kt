b0ab00a0b8b158e6169cb353a18c5658dfa4c213
kiviarib <kiviamariana1@gmail.com>
2021-07-06 17:44:27 -0300

    create person class

---
+++ fundamentos_oo/src/main/kotlin/Pessoa.kt
@@ -0,0 +1,18 @@
+class Pessoa {
+
+    var nome: String = "Kivia"
+    var cpf: String = "123.123.123-12"
+    var endereco: String = "Pedro Alvares Cabral"
+
+}
+fun main() {
+    val Kivia = Pessoa()
+
+    println(Kivia.nome)
+    println(Kivia.cpf)
+    println(Kivia.endereco)
+
+}
+
+
+

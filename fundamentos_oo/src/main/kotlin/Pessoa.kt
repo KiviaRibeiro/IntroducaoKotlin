04e4a287a88901557949b26d7d09f37b4282d4c0
kiviarib <kiviamariana1@gmail.com>
2021-07-06 22:05:36 -0300

    update to talk about class members

--- fundamentos_oo/src/main/kotlin/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/Pessoa.kt
@@ -3,14 +3,16 @@ class Pessoa {
     var nome: String = "Kivia"
     var cpf: String = "123.123.123-12"
             private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 fun main() {
     val Kivia = Pessoa()
 
-    println(Kivia)
-    println(Kivia.nome)
-    println(Kivia.cpf)
-
+    println(Kivia.pessoaInfo())
 }
 
 

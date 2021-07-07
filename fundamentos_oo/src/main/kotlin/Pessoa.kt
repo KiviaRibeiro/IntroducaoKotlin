24ab4ffb7ff34300d1e0fbc0f489340bf47fc028
kiviarib <kiviamariana1@gmail.com>
2021-07-06 21:37:45 -0300

    update to apply the private modifier to set function of cpf property

--- fundamentos_oo/src/main/kotlin/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/Pessoa.kt
@@ -2,15 +2,14 @@ class Pessoa {
 
     var nome: String = "Kivia"
     var cpf: String = "123.123.123-12"
-    var endereco: String = "Pedro Alvares Cabral"
-
+            private set
 }
 fun main() {
     val Kivia = Pessoa()
 
+    println(Kivia)
     println(Kivia.nome)
     println(Kivia.cpf)
-    println(Kivia.endereco)
 
 }
 
